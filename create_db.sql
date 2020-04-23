8d0e45a1eb124d0b968a633fff49a6d4d165d677
Avrumi Friedman <a.friedman07@gmail.com>
2020-04-22 22:21:37 -0400

    Added db script

---
+++ create_db.sql
@@ -0,0 +1,40 @@
+USE [SimpleAdsAuth]
+GO
+/****** Object:  Table [dbo].[Ads]    Script Date: 4/22/2020 10:20:47 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Ads](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [varchar](max) NOT NULL,
+	[PhoneNumber] [varchar](50) NOT NULL,
+	[DateCreated] [datetime] NOT NULL,
+	[UserId] [int] NOT NULL,
+ CONSTRAINT [PK_Ads] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 4/22/2020 10:20:47 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+	[PasswordHash] [varchar](255) NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Ads]  WITH CHECK ADD  CONSTRAINT [FK_Ads_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Ads] CHECK CONSTRAINT [FK_Ads_Users]
+GO
